ae7afff5248e63b76193dce3a45ffe51aaf38f57
ISVezhichanin <isvezhichanin@greenatom.ru>
2024-06-05 17:26:40 +0300

    Created number game1

--- 002_console_game.js
+++ 002_console_game.js
@@ -1,3 +1,4 @@
+#!/usr/bin/env node
 const readline = require('node:readline');
 const { stdin: input, stdout: output } = require('node:process');
 const rl = readline.createInterface({ input, output });
